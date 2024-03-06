28d3c3f62c2afe6d0a0580e78854b2485c0c9ab8
Hannd15 <lejhernandez08@gmail.com>
2024-03-05 20:25:42 -0500

    Trabajo en clase 05/03/2024

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -44,56 +44,16 @@
           </div>
         </div>
     </nav>
-    <div id="carouselExampleIndicators" class="carousel slide">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div class="carousel-inner" >
-          <div class="carousel-item active">
-            <img src="{{ asset('imgs/slider-1.png') }}" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('imgs/slider-2.png') }}" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="{{ asset('imgs/slider-3.png') }}" class="d-block w-100" alt="...">
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-    </div>
     <h1 class="text-center m-5">Productos</h1>
-    <div class="container text-center">
+    <div class="container">
         <div class="row">
-            <div class="card col-md-3" style="width: 18rem; col-md-3">
-                <img src="{{asset('imgs/bomDia.jpg')}}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Nice cat :3</h5>
-                  <p class="card-text">$Un morbillon de pesos</p>
-                  <a href="#" class="btn btn-primary">Añadir al carrito</a>
-                </div>
-              </div>
-              <div class="card col-md-3" style="width: 18rem; col-md-3">
-                <img src="{{asset('imgs/bomDia.jpg')}}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Nice cat :3</h5>
-                  <p class="card-text">$Un morbillon de pesos</p>
-                  <a href="#" class="btn btn-primary">Añadir al carrito</a>
-                </div>
-              </div>
-            <div class="col-md-3">
-                <p>momazos</p>
+            <div class="col-md-8">
+                <img src="{{asset('imgs/bomDia.jpg')}}" alt="Rude Cat">
             </div>
-            <div class="col-md-3">
-                <p>momazos</p>
+            <div class="col-md-4 text-left">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit amet nihil debitis nobis, veniam voluptates quaerat ipsum cumque recusandae ea nulla aperiam maxime magnam, molestiae soluta sint maiores voluptatem veritatis!</p>
+                <h3>$Un morbillon de pesos</h3>
+                <button type="button" class="btn btn-primary">Añadir al carrito</button>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products', [ProductController::class,'index']);
+
+Route::get('/products/detail', function () {
+    return view('detail');
 });
